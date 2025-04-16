69fff6c213ca4c2062b6d8d66388fa6b594a9fd8
Daniel Clavijo Coca <dclavijo@opennebula.io>
2025-04-16 16:54:11 +0200

    Update VM and service templates

--- terraform/opennebula/modules/service_template/main.tf
+++ terraform/opennebula/modules/service_template/main.tf
@@ -1,10 +1,10 @@
 resource "opennebula_service_template" "faas" {
-  name     = "Function"
+  name     = var.name
   template = <<EOF
     {
       "TEMPLATE": {
         "BODY": {
-          "name": "Function",
+          "name": "${var.name}",
           "deployment": "straight",
           "roles": [
             {
@@ -12,7 +12,15 @@ resource "opennebula_service_template" "faas" {
               "cardinality": 1,
               "vm_template": ${var.faas_vm_template_id}
             }
-          ]
+          ],
+          "user_inputs": {
+            "COGNIT_BRANCH": "O|text|Serverless Runtime commit available at https://github.com/SovereignEdgeEU-COGNIT/serverless-runtime| |main",
+            "COGNIT_BROKER": "M|text|Endpoint where the RabbitMQ broker designated to the Edge Cluster can be reached| |",
+            "COGNIT_FLAVOUR": "M|text|Flavour the SR will be responsible for. It will listen requests issued to that flavour queue.| |${var.name}"
+          },
+          "description": {
+            "Service to handle a scaling group of SR VMs. Will create a COGNIT_FLAVOUR queue in the given COGNIT_BROKER endpoint, create an SR API on each FAAS VM and bind each VM to the COGNIT_FLAVOUR queue"
+          }
         }
       }
     }
--- terraform/opennebula/modules/vm_template/main.tf
+++ terraform/opennebula/modules/vm_template/main.tf
@@ -9,13 +9,22 @@ resource "opennebula_template" "faas" {
   memory = 1024
 
   context = {
+    COGNIT_BRANCH="$COGNIT_BRANCH",
+    COGNIT_BROKER="$COGNIT_BROKER",
+    COGNIT_FLAVOUR="$COGNIT_FLAVOUR",
     NETWORK             = "YES"
     HOSTNAME            = "$NAME"
     READY_SCRIPT        = "ss -tln src :8000 | grep 8000"
     REPORT_READY        = "YES"
     SSH_PUBLIC_KEY      = "$USER[SSH_PUBLIC_KEY]"
-    START_SCRIPT_BASE64 = "Y2QgL3Jvb3Qvc2VydmVybGVzcy1ydW50aW1lCmdpdCBjaGVja291dCBzcl9wcm9tX21ldHJpY3MKc291cmNlIHNlcnZlcmxlc3MtZW52L2Jpbi9hY3RpdmF0ZSCZY2QgYXBwCnB5dGhvbjMgbWFpbi5weSAm"
+    START_SCRIPT_BASE64 = "c291cmNlIC92YXIvcnVuL29uZS1jb250ZXh0L29uZV9lbnYKY2QgL3Jvb3Qvc2VydmVybGVzcy1ydW50aW1lCmdpdCByZW1vdGUgcmVtb3ZlIG9yaWdpbgpnaXQgcmVtb3RlIGFkZCAib3JpZ2luIiBodHRwczovL2dpdGh1Yi5jb20vU292ZXJlaWduRWRnZUVVLUNPR05JVC9zZXJ2ZXJsZXNzLXJ1bnRpbWUuZ2l0CmdpdCBmZXRjaApnaXQgY2hlY2tvdXQgJENPR05JVF9CUkFOQ0gKc291cmNlIHNlcnZlcmxlc3MtZW52L2Jpbi9hY3RpdmF0ZQpwaXAgaW5zdGFsbCAtciByZXF1aXJlbWVudHMudHh0CmNkIGFwcC8KLi9lbnRyeXBvaW50LnNo"
     TOKEN               = "YES"
+  }
+
+  user_inputs = {
+    COGNIT_BRANCH="O|text|Serverless Runtime commit available at https://github.com/SovereignEdgeEU-COGNIT/serverless-runtime| |main",
+    COGNIT_BROKER="M|text|Endpoint where the RabbitMQ broker designated to the Edge Cluster can be reached| |",
+    COGNIT_FLAVOUR="M|text|Flavour the SR will be responsible for. It will listen requests issued to that flavour queue.| |${var.name}"
   }
 
   graphics {
